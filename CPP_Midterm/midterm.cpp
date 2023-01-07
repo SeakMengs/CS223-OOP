73b583022e1a3e0cf91439466b0efc3ff7932404
Yato <54373229+SeakMengs@users.noreply.github.com>
2023-01-07 20:52:34 +0700

    add constructor to database

--- CPP_Midterm/midterm.cpp
+++ CPP_Midterm/midterm.cpp
@@ -1,8 +1,5 @@
-/*
- * @Author: Seakmeng Hor, Bunlong Prak, Sopheaknita Chea
- * Course: CS201, Section 1
- * Midterm Mission
- */
+/// @author: Seakmeng Hor, Bunlong Prak, Sopheaknita Chea
+/// @course: CS201 Section 1
 
 #include <bits/stdc++.h>
 
@@ -11,7 +8,14 @@ using namespace std;
 
 class Database {
    public:
-
+    // default constructor of array size of 10
+    Database() {
+        Employee employee[10];
+    }
+    // constructor with fixed size
+    Database(int arraySize) {
+        Employee employee[arraySize];
+    }
 };
 
 /// @brief Employee class to store data, access data
@@ -24,34 +28,36 @@ class Employee {
     string position;
 
    public:
-   //* Getter and Setter -------------------------------------------------------------------------------------------------------------------------
-    int getAge() const { return age; }
+    //* Getter and Setter ------------------------------------------------------
+    int getAge() const { return this->age; }
 
-    void setAge(int age) { age = age; }
+    void setAge(int age) { this->age = age; }
 
-    string getId() const { return id; }
+    string getId() const { return this->id; }
 
-    void setId(int id) { id = id; }
+    void setId(int id) { this->id = id; }
 
-    string getName() const { return name; }
+    string getName() const { return this->name; }
 
-    void setName(string name) { name = name; }
+    void setName(string name) { this->name = name; }
 
-    string getGender() const { return gender; }
+    string getGender() const { return this->gender; }
 
-    void setGender(string gender) { gender = gender; }
+    void setGender(string gender) { this->gender = gender; }
 
-    string getPosition() const { return position; }
+    string getPosition() const { return this->position; }
 
-    void setPosition(string position) { position = position; }
-   //* End of Getter and Setter -------------------------------------------------------------------------------------------------------------------
+    void setPosition(string position) { this->position = position; }
+    //* End of Getter and Setter -----------------------------------------------
 
+    // function to print out the data
     void printData() {
-      cout << age << "\t\t" << id << "\t\t" << name << "\t\t" << gender << "\t\t" << position << endl;
+        cout << age << "\t\t" << id << "\t\t" << name << "\t\t" << gender
+             << "\t\t" << position << endl;
     }
 };
 
-int main() {
-
-   return 0; 
+int main() { 
+    Database database1;
+    return 0;
 }
